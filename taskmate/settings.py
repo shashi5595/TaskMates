eb99d8988812cb364b374b8a15a78b745117045e
Shashi <sashi6897@gmail.com>
2021-02-19 13:48:35 +0530

    Uploading taskmate after heroku setting

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,4 +132,6 @@ LOGIN_REDIRECT_URL="todolist"
 
 LOGIN_URL="login"
 
-#LOGOUT_REDIRECT_URL="todolist"
+
+django_heroku.settings(locals())
+
